2641e74b8787c5145dccda33eb35d9d720a8801f
ajithmemana <ajithmemana@outlook.com>
2013-06-06 19:35:43 +0530

    markers added

--- src/com/droidschools/mapsample/MapActivity.java
+++ src/com/droidschools/mapsample/MapActivity.java
@@ -1,14 +1,20 @@
 package com.droidschools.mapsample;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 
 import com.droischools.mapsample.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.GoogleMap.InfoWindowAdapter;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 public class MapActivity extends FragmentActivity {
 
@@ -16,12 +22,19 @@ public class MapActivity extends FragmentActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_map);
+		LatLng koratty = new LatLng(10.26127,76.35465);
+		LatLng trichurRound = new LatLng(10.527549,76.214487);
+		LatLng infopark = new LatLng(10.268992,76.354183);
 		GoogleMap map = ((SupportMapFragment) getSupportFragmentManager()
 				.findFragmentById(R.id.map)).getMap();
-		map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-		map.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title(
-				"Marker"));
+		//map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+		map.addMarker(new MarkerOptions().position(koratty).title(
+				"Koratty zodiac bar").snippet("best drinks available ").anchor(0, 0));
+		map.addMarker(new MarkerOptions().position(infopark).title("Infopark koratty"));
+		map.addPolyline(new PolylineOptions().add(koratty, infopark , trichurRound).width(5).color(Color.RED));
+		// Shows my point on map
 		map.setMyLocationEnabled(true);
+		map.moveCamera(CameraUpdateFactory.newLatLngZoom(koratty, 15));
 	}
 
 	@Override
